aa2d9c835606ee75ddc45d402e97b2d37c77dea6
Damon <dmarcr1997@gmail.com>
2020-03-25 22:25:16 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
+
+  def user_attributes=(user_attributes)
+    new_user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].empty?
+    self.user = new_user unless new_user.nil?
+  end 
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create(name:"Awesome")
+Category.create(name:"Sweet")
+Category.create(name:"Gnarley")
+User.create(username: "User1", email: "user1@email.com")
+User.create(username: "User2", email: "user2@email.com")
+User.create(username: "User3", email: "user3@email.com")
+Post.create(title:"Post1", content: "first content")
+Post.create(title:"Post2", content: "second content")
+Post.create(title:"Post3", content: "third content")
+Comment.create(content:"Comment 1", user_id: 1, post_id: 1)
+Comment.create(content:"Comment 2", user_id: 2, post_id: 2)
+Comment.create(content:"Comment 3", user_id: 3, post_id: 3)
+PostCategory.create(post_id:1, category_id:1)
+PostCategory.create(post_id:2, category_id:2)
+PostCategory.create(post_id:3, category_id:3)
+
+
+
